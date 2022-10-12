ed50f7cce049778f1ef0a71dfc9f9d1f40fc0a80
giuseppe filannino <peppe.f94@hotmail.it>
2022-10-12 10:56:35 +0200

    props-02

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ export class App extends React.Component {
         return (
             <div>
                 <HelloWorld />
-                <Welcome name="Giuseppe" />
+                <Welcome  />
 
             </div>
         )
--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -2,12 +2,20 @@ import React from "react";
 
 
 export class Welcome extends React.Component {
+
+
     render() {
         return (
             <div>
                 <p>Welcome, {this.props.name} !</p>
-            
+
             </div>
         )
     }
+}
+
+
+Welcome.defaultProps = {
+    name: "Giuseppe"
+
 }
