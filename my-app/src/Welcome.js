edefb8ad9ad42be68d91171dd275eb4b47101f9b
giuseppe filannino <peppe.f94@hotmail.it>
2022-10-12 11:56:14 +0200

    conditional_rendering-03

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -10,7 +10,7 @@ export class Welcome extends React.Component {
         
             <div>
                 <p>Welcome, {this.props.name} !</p>
-               { Age.defaultProps.age  && <Age />} 
+               { Age.defaultProps.age > 18 && Age.defaultProps.age < 65 && <Age />} 
              
 
             </div>
