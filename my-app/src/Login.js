9d4f36da948ccf9f0345a0950dd46ef02e5aa212
giuseppe filannino <peppe.f94@hotmail.it>
2022-10-14 17:26:01 +0200

    forms-03

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -7,7 +7,7 @@ export class Login extends React.Component {
     state = {
         username: '',
         password: '',
-        remember: '',
+        remember: false,
         loginStatus: true
 
     }
@@ -43,7 +43,7 @@ export class Login extends React.Component {
 
                 <label>Insert username:</label><input name="username" value={this.state.username} onChange={this.handleInputChange}></input><br />
                 <label>Insert password:</label><input name="password" value={this.state.password} onChange={this.handleInputChange} type="password"></input><br />
-                <label>Checkbox:</label><input name ="remember" value={this.state.remember} onChange={this.handleInputChange} type="checkbox"></input><br />
+                <label>Checkbox:</label><input name ="remember" checked={this.state.remember} onChange={this.handleInputChange} type="checkbox"></input><br />
                 <button name="login" type="button" onClick={this.handleLogin} disabled={this.state.loginStatus}>Login</button>
 
                 {/* status = {this.state.status} */}
