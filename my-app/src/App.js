59f5789a50e936819abf7f9f0489337363cafc5b
giuseppe filannino <peppe.f94@hotmail.it>
2022-10-19 10:36:33 +0200

    context-01

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,8 @@ import { Login } from './Login';
 import { UncontrolledLogin } from './UncontrolledLogin';
 import { TodoList } from './TodoList';
 import { Container } from './Container';
+import { LanguageContext } from './LanguageContext';
+import {DisplayLanguage} from './DisplayLanguage'
 
 
 const login = (dati) => {
@@ -17,30 +19,51 @@ const login = (dati) => {
 }
 
 export class App extends React.Component {
+    state = {
+        language: 'en'
+    }
+
+    handleLanguageChange = (event) => {
+        this.setState({
+            language: event.target.value,
+        })
+    }
+
+
     render() {
         return (
+            <div>
+                <Container title={'React exercises'}>
+                    <HelloWorld /> <hr></hr>
+                    <Welcome /> <hr></hr>
+                    <Counter /> <hr></hr>
+                    <ClickCounter /> <hr></hr>
+                    <ClickTracker /> <hr></hr>
+                    <InteractiveWelcome /> <hr></hr>
+                    <Login onLogin={login} /> <hr></hr>
+                    <UncontrolledLogin /> <hr></hr>
+                    <TodoList
+                        render={(items, handleRemoveElement) => {
+                            return (
+                                <ul>
+                                    {items.map((item) => <li>{item}<button id={item} onClick={handleRemoveElement}>Remove</button></li>)}
 
-            <Container title={'React exercises'}>
-                <HelloWorld /> <hr></hr>
-                <Welcome /> <hr></hr>
-                <Counter /> <hr></hr>
-                <ClickCounter /> <hr></hr>
-                <ClickTracker /> <hr></hr>
-                <InteractiveWelcome /> <hr></hr>
-                <Login onLogin={login} /> <hr></hr>
-                <UncontrolledLogin /> <hr></hr>
-                <TodoList
-                    render={(items, handleRemoveElement) => {
-                        return (
-                            <ul>
-                                {items.map((item) => <li>{item}<button id={item} onClick={handleRemoveElement}>Remove</button></li>)}
 
+                                </ul>)
+                        }}
+                    >
+                    </TodoList>
+                </Container >
+                <h2>Context</h2>
 
-                            </ul>)
-                    }}
-                >
-                </TodoList>
-            </Container >
+                <select value={this.state.language} onChange={this.handleLanguageChange}>
+                    <option value='en'>ENGLISH</option>
+                    <option value='it'>ITALIANO</option>
+                </select>
+                <LanguageContext.Provider value={this.state.language}>
+                    <DisplayLanguage />
+                </LanguageContext.Provider>
+            </div>
         )
     }
 }
