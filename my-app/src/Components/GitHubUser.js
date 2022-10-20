62c7ac1d1799ac970a98e0ac3f466091b9e8debd
giuseppe filannino <peppe.f94@hotmail.it>
2022-10-20 17:25:33 +0200

    custom-hook-04

--- my-app/src/Components/GitHubUser.js
+++ my-app/src/Components/GitHubUser.js
@@ -3,17 +3,19 @@ import { useGitHubUser } from "./useGitHubUser"
 
 
 export function GitHubUser({ username }) {
-    const { data, error, onFetchUser } = useGitHubUser(username)
+    const { data, error, loading, onFetchUser } = useGitHubUser(username)
 
     useEffect(() => {
         onFetchUser(username)
-    })
+
+    }, [username])
 
 
 
 
 
     return <div>
+        {loading && <h1>Loading data...</h1>}
         {error && <h1>There has been an error</h1>}
         {data && <h1>The user's name is: {data.name}</h1>}
     </div>
--- my-app/src/Components/useGitHubUser.js
+++ my-app/src/Components/useGitHubUser.js
@@ -2,13 +2,13 @@ import { useState } from "react"
 
 export function useGitHubUser() {
     const [data, setData] = useState(null)
-  
+    const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
 
     async function fetchGitHubUser(username) {
 
-
+        setLoading(true)
         setError(null)
         try {
             const response = await fetch(`https://api.github.com/users/${username}`)
@@ -24,17 +24,19 @@ export function useGitHubUser() {
             setError(err)
             setData(null)
         } finally {
+            setLoading(false)
+               
 
-         
         }
 
     }
 
- 
 
-    return{
+
+    return {
         data,
         error,
+        loading,
         onFetchUser: fetchGitHubUser
     }
 }
