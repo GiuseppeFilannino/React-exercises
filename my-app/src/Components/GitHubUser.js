a6fa3cdca30660fa86298d4fdca0f59f31dc8c55
giuseppe filannino <peppe.f94@hotmail.it>
2022-10-20 11:47:22 +0200

    useEffect-03

--- my-app/src/Components/GitHubUser.js
+++ my-app/src/Components/GitHubUser.js
@@ -1,19 +1,47 @@
-import { useGitHubUser } from "./useGitHubUser"
+import { useEffect, useState } from "react"
 
 
 export function GitHubUser({ username }) {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
-    const { data, loading, error, onFetchUser } = useGitHubUser(username)
+
+    async function fetchGitHubUser(username) {
 
 
-    function handleGetUserData(){
-    
-    onFetchUser(username)
-}
+        setLoading(true)
+        setError(null)
+        try {
+            const response = await fetch(`https://api.github.com/users/${username}`)
+            const json = await response.json()
+
+            if (response.status !== 200) {
+                setError(new Error())
+            }
+
+            setData(json)
+
+        } catch (err) {
+            setError(err)
+            setData(null)
+        } finally {
+
+            setLoading(false)
+        }
+
+    }
+
+    useEffect(() =>{
+    fetchGitHubUser(username)
+   } ,[username])
+
+
+
 
 
     return <div>
-        <button onClick={handleGetUserData}>Load User data</button>
+       
         {loading && <h1>Loading...</h1>}
         {error && <h1>There has been an error</h1>}
         {data && <h1>{data.name}</h1>}
