ee68bebd696526d422b9e52ad256965890f000f9
giuseppe filannino <peppe.f94@hotmail.it>
2022-10-20 09:38:55 +0200

    forms-02

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -4,6 +4,26 @@ import React from "react";
 
 export class Login extends React.Component {
 
+    state = {
+        username: '',
+        password: '',
+        remember: false
+    }
+
+    handleInputChange = (event) => {
+        const value = event.target.value
+        const name = event.target.name
+        const type = event.target.type
+        const checked = event.target.checked
+        
+        console.log(event.target.type)
+
+        this.setState({
+            [name]: type === 'checked' ? checked : value
+        })
+
+    }
+
 
 
     render() {
@@ -12,11 +32,11 @@ export class Login extends React.Component {
 
             <div>
 
-                <label>Insert username:</label><input></input><br/>
-                <label>Insert password:</label><input type="password"></input><br/>
-                <label>Checkbox:</label><input type="checkbox"></input>
-                    
-                    
+                <label>Insert username:</label><input value={this.state.username} name='username' onChange={this.handleInputChange}></input><br />
+                <label>Insert password:</label><input value={this.state.password} name='password' onChange={this.handleInputChange} type="password"></input><br />
+                <label>Checkbox:</label><input checked={this.state.remember} name='remember' onChange={this.handleInputChange} type="checkbox"></input>
+
+
 
 
             </div>
