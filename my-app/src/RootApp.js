72c0fbcd664078226e8cab0c6e2facddcf0c1390
giuseppe filannino <peppe.f94@hotmail.it>
2022-10-22 11:58:04 +0200

    react-router-02

--- my-app/src/RootApp.js
+++ my-app/src/RootApp.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
+import { Counter } from './Components/Counter';
 import { Welcome } from './Components/Welcome';
 
 export function RootApp() {
     return (
         <Routes >
-            <Route path='/' element={<Welcome  name='Jimmy'/>}>
+            <Route path='/' element={<Welcome  name='Jimmy'/>} />
+            <Route path='counter' element={<Counter  initialValue={0}/>} />
 
-            </Route>
+            
         </Routes>
     )
 }
